6bbd8c860eccefa248dc1af5cf840d1feff1258c
Gregg Coppen <gregg@iaminawe.com>
2013-02-10 15:03:34 +0200

    Trying to fix o1
    Signed-off-by: Gregg Coppen <gregg@iaminawe.com>

--- cookbooks/barracuda/recipes/default.rb
+++ cookbooks/barracuda/recipes/default.rb
@@ -13,48 +13,58 @@ execute "Run the BOA Installer o1" do
   command "boa in-stable local gregg@iaminawe.com mini"
 end
 
+#execute "Run the BOA Installer o2" do
+#  command "boa in-stable local gregg@iaminawe.com aegir.local o2 mini"
+#end
 
-  user "o1" do
+#execute "Run the BOA Installer o3" do
+ # command "boa in-stable local lars@intraface.dk aegir.local o3 max"
+#end
+
+(1..1).each do |boa_user|
+
+  user "o#{boa_user}" do
     supports :manage_home => true
-    home "/data/disk/o1"
+    home "/data/disk/o#{boa_user}"
     shell "/bin/bash"
   end
 
-  directory "/data/disk/o1/.ssh" do
-    owner "o1"
+  directory "/data/disk/o#{boa_user}/.ssh" do
+    owner "o#{boa_user}"
     group "users"
     mode 00700
     recursive true
   end
 
   execute "Add ssh key to user" do
-    command "ssh-keygen -b 4096 -t rsa -N \"\" -f /data/disk/o1/.ssh/id_rsa"
-    creates "/data/disk/o1/.ssh/id_rsa"
+    command "ssh-keygen -b 4096 -t rsa -N \"\" -f /data/disk/o#{boa_user}/.ssh/id_rsa"
+    creates "/data/disk/o#{boa_user}/.ssh/id_rsa"
   end
 
-  file "/data/disk/o1/.ssh/id_rsa" do
-    owner "o1"
+  file "/data/disk/o#{boa_user}/.ssh/id_rsa" do
+    owner "o#{boa_user}"
     group "users"
     mode 00600
   end
   
-  file "/data/disk/o1/.ssh/id_rsa.pub" do
-    owner "o1"
+  file "/data/disk/o#{boa_user}/.ssh/id_rsa.pub" do
+    owner "o#{boa_user}"
     group "users"
     mode 00600
   end  
 
   # Only necessary as long as there is a but
- # remote_file "/tmp/fix-remote-import-hostmaster-o1.patch" do
- #   source "https://raw.github.com/lsolesen/boa-vagrant/master/patches/fix-remote-import-hostmaster-o1.patch"
-  #  mode 00755
- # end
+  remote_file "/tmp/fix-remote-import-hostmaster-o#{boa_user}.patch" do
+    source "https://raw.github.com/lsolesen/boa-vagrant/master/patches/fix-remote-import-hostmaster-o#{boa_user}.patch"
+    mode 00755
+  end
 
- # execute "Apply Remote Import hostmaster patch" do
-#    cwd "/data/disk/o1/.drush/provision/remote_import"
-#    command "patch -p1 < /tmp/fix-remote-import-hostmaster-o1.patch"
- # end
+  execute "Apply Remote Import hostmaster patch" do
+    cwd "/data/disk/o#{boa_user}/.drush/provision/remote_import"
+    command "patch -p1 < /tmp/fix-remote-import-hostmaster-o#{boa_user}.patch"
+  end
 
+end
 
 execute "Run BOA Tool to fix permissions" do
   user "root"
@@ -66,25 +76,3 @@ end
 execute "Rebuild VirtualBox Guest Additions" do
   command "sudo /etc/init.d/vboxadd setup"
 end
-
-
-# Setup the relevant file system packages to share mounted folders
-
-#package "nfs-kernel-server"
-#package "nfs-common"
-#package "rpcbind"
-#template '/etc/exports' do
-#  source "exports.erb"
-#  owner "root"
-#  group "root"
-#  mode "0644"
-#  action :create
-#end
-
-#script "nfs_restart" do
- # interpreter "bash"
-#  user "root"
-#  code <<-EOH
-#/etc/init.d/nfs-kernel-server restart
-#  EOH
-#end
